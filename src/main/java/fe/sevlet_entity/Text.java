0ffba2822814312e1ed7a3bfda2617be1266a7a0
yevgeniyfilonov <Filonov87@gmail.com>
2014-07-03 17:03:38 +0600

    job

--- src/main/java/fe/sevlet_entity/Text.java
+++ src/main/java/fe/sevlet_entity/Text.java
@@ -1,5 +1,9 @@
 package fe.sevlet_entity;
 
+import java.util.List;
+
 public class Text {
+    List<Paragraph>Paragraph;//парсинг джинерики коллекция
+    //найти класс в яве который разбивает класс
 
 }
